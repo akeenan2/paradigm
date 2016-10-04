26c2036be0cbffbe2e46729be24564ffd2d4a02b
akeenan2 <ann.keenan2@gmail.com>
2016-10-04 11:53:51 -0400

    add initial webpage

--- paradigm/settings.py
+++ paradigm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['dsg1.crc.nd.edu']
 # Application definition
 
 INSTALLED_APPS = [
+    'zoo.apps.ZooConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

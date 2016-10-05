2fa004d22ca1491664eb27dafbad880e8c3faaaf
akeenan2 <ann.keenan2@gmail.com>
2016-10-04 22:09:45 -0400

    add documentation

---
+++ zoo/static/js/custom.js
@@ -0,0 +1,11 @@
+/* 
+    custom.js
+    description: custom js style
+    created by: ann keenan
+*/
+
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+});

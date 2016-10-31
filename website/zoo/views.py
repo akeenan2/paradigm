c0ffec00cf69139f3ad507dde044aa22c74e6e79
akeenan2 <ann.keenan2@gmail.com>
2016-10-30 20:47:22 -0400

    try

--- website/zoo/views.py
+++ website/zoo/views.py
@@ -13,7 +13,7 @@ def list_zoos(request):
 
 def zoo(request,zoo_id):
     zoo = Zoo.objects.get(id=zoo_id)
-    list_species = Exhibit.objects.select_related('species').select_related('zoo_name')
+    exhibits = Exhibit.objects.filter(zoo_name__zoo_name=zoo.zoo_name).select_related('species')
     #list_species = Species.objects.raw('SELECT * FROM Species, Exhibit WHERE Species.species=Exhibit.species');
     return render(request,'zoo/zoo.html',{'zoo':zoo},{'list_species':list_species})
 

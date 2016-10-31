3c5a9c5339a0af96e57a775816c77cb1e7e2bdf6
akeenan2 <ann.keenan2@gmail.com>
2016-10-31 00:11:00 -0400

    add views

--- website/zoo/views.py
+++ website/zoo/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Zoo,Species,Biome,Exhibit,Family
 from django.db import connection, connections
+
 def index(request):
     return render(request,'zoo/index.html')
 
@@ -22,10 +23,17 @@ def zoo(request,zoo_id):
     return render(request,'zoo/zoo.html',{'zoo':zoo,'list_species':list_species})
 
 def update_zoo(request,zoo_id):
+    zoo = Zoo.objects.get(id=zoo_id)
     if request.method == 'POST':
-        print 'post'
-    zoo= Zoo.objects.get(id=zoo_id)
+        save_zoo(request=request,zoo_id=zoo_id)
+        return HttpResponseRedirect('/zoo/'+zoo_id+'/')
     return render(request,'zoo/update_zoo.html',{'zoo':zoo})
+
+def list_species(request):
+    list_species = Species.objects.all()
+    for species in list_species:
+        species.common_name = species.common_name.split(';')[0]
+    return render(request,'zoo/list_species.html',{'list_species':list_species})
 
 def species(request,species_id):
     species = Species.objects.get(id=species_id)
@@ -35,8 +43,10 @@ def species(request,species_id):
         list_zoos = cursor.fetchall()
     return render(request,'zoo/species.html',{'species':species,'species_name':species_name,'list_zoos':list_zoos})
 
-def list_species(request):
-    list_species = Species.objects.all()
-    for species in list_species:
-        species.common_name = species.common_name.split(';')[0]
-    return render(request,'zoo/list_species.html',{'list_species':list_species})
+def update_species(request,species_id):
+    species = Zoo.objects.get(id=species_id)
+    if request.method == 'POST':
+        save_zoo(request=request,species_id=species_id)
+        return HttpResponseRedirect('/species/'+species_id+'/')
+    species_name = species.common_name.split(';')[0]
+    return render(request,'zoo/update_species.html',{'species':species,'species_name':species_name})

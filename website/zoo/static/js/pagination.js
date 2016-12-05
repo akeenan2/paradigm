6af9ad97626299987169d3fe5c47843c9d46ebb5
akeenan2 <ann.keenan2@gmail.com>
2016-12-05 10:15:11 -0500

    fix page load time and db

--- website/zoo/static/js/pagination.js
+++ website/zoo/static/js/pagination.js
@@ -1,14 +1,10 @@
 // pagination.js
 $(document).ready(function() {
-    change_names();
-});
-
-function change_names() {
     var obj = document.getElementsByClassName('common_name');
     for (i=0;i<obj.length;i++) {
         obj[i].innerHTML = obj[i].innerHTML.split(';')[0];;
     }
-}
+});
 
 $(document).ready(function() {
     $('#pagination').DataTable();

0043f2d4e6fbe90e4429fd91224c29228548f1b8
akeenan2 <ann.keenan2@gmail.com>
2016-12-05 10:05:04 -0500

    fix common names to display only one name

--- website/zoo/static/js/main.js
+++ website/zoo/static/js/main.js
@@ -1,6 +1,8 @@
+// main.js
 function change_link(obj,baseurl) {
     var original = obj.innerHTML; // without underscores
     var fixed = original.replace(/ /g,'_'); // with underscores
+    obj.href = fixed; 
     window.location.href = baseurl+fixed+'/';
 }
 
@@ -21,9 +23,6 @@ function hide_overlay(obj) {
     document.getElementById(id).className = "overlay hide";
 }
 
-$(document).ready(function() {
-    $('#pagination').DataTable();
-});
 
 $('input.limit-habitats').on('change', function(e) {
 // limit to 10
@@ -41,12 +40,4 @@ $('input.limit-regions').on('change', function(e) {
     } else {
         $('.limit-regions').removeAttr("disabled");
     }
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
 });
